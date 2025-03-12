f83513ca9b57fdd48e40ce97683ff87521896fbb
kaviyamanoharan <102371722+kaviyamanoharan@users.noreply.github.com>
2025-03-12 11:59:04 +0530

    Update dynamic_agent_3databasefull.py

--- dynamic_agent_3databasefull.py
+++ dynamic_agent_3databasefull.py
@@ -1,3 +1,9 @@
+__import__('pysqlite3')
+
+import sys
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+ 
 import pandas as pd
 from sqlalchemy import create_engine, text
 import streamlit as st
